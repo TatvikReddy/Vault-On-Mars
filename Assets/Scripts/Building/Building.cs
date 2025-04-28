adffa5daa7552769b4bce5e22039bdb96caeb6c6
Nafiz Rahman <100969321+nafizR@users.noreply.github.com>
2025-04-27 20:31:48 -0500

    Scrum 48 technology tree basics (#5)
    * Buttons and Title
    
    Without  Background Image or Link to Game
    
    * Re-add changes
    
    I forgot to save earlier.
    
    * Added Settings Screen and Exit Button
    
    * Fixed X button of Settings Screen
    
    * Added Basics of Technology Tree
    
    Added UI elements for technology tree and much of the functions of it as well. We still need to design and completely finish functionality but that will be later after ideas for technology tree progression is complete.
    
    * Fixed Issues on Starting Screen
    
    The start screen canvas was not sized properly or anchored right so that was fixed. Also the hierarchy of the game objects did not make sense so they were organized more clearly. Fixed an issue with the start screen settings exit button not appearing after closing settings screen once. Organized the folder system of scripts, prefabs, and materials better.
    
    ---------
    
    Co-authored-by: snsorl <suryasolaiappan@gmail.com>

--- Assets/Scripts/Building/Building.cs
+++ Assets/Scripts/Building/Building.cs
@@ -7,12 +7,21 @@ public class Building : Interactable
     public string buildingName;
     // Blueprint of the building (Contains the Prefab, position offsets, and costs)
     public BuildingBlueprint blueprint;
+
+    public GameObject menuCanvas;
+
+    public bool onDisplayCanMove = true;
     
     // Add NPC array here later
     public override void Interact()
     {
         // Add functionality of displaying building details when interacted with
         Debug.Log("Interacted with " + buildingName);
+
+        if (menuCanvas != null)
+        {
+            enableMenu(onDisplayCanMove);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -31,5 +40,17 @@ public class Building : Interactable
             GameManager.instance.currentInteractable = null;
             GameManager.instance.canInteract = false;
         }
+    }
+
+    public void enableMenu(bool freeMovement)
+    {
+        GameManager.instance.canMove = freeMovement;
+        menuCanvas.SetActive(true);
+    }
+
+    public void disableMenu()
+    {
+        GameManager.instance.canMove = true;
+        menuCanvas.SetActive(false);
     }
 }
---
+++ Assets/Scripts/UI/QuitGame.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    public void Quitgame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
