cc6a2d19e4f643f7125f8d6b18d4e1bcf0aeadd0
Nafiz Rahman <100969321+nafizR@users.noreply.github.com>
2025-05-05 23:54:31 -0500

    Updated Test Scene Explanations
    Added some UI text bits to the test scene to make it easier for people to see what we have made in there.

--- Assets/Scripts/Player/PlayerInventory.cs
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -1,16 +1,28 @@
 using System;
 using UnityEngine;
 using System.Collections.Generic;
+using TMPro;
 
 public class PlayerInventory : MonoBehaviour
 {
     Dictionary<ResourceType, int> resources = new Dictionary<ResourceType, int>();
 
+    public TMP_Text metalText;
+    public TMP_Text waterText;
+    public TMP_Text energyText;
+    
     private void Start()
     {
         resources[ResourceType.Metal] = 0;
         resources[ResourceType.Water] = 0;
         resources[ResourceType.Energy] = 0;
+    }
+
+    private void Update()
+    {
+        metalText.text = printResource(ResourceType.Metal);
+        waterText.text = printResource(ResourceType.Water);
+        energyText.text = printResource(ResourceType.Energy);
     }
 
     public int getResource(ResourceType resource)
@@ -23,5 +35,24 @@ public class PlayerInventory : MonoBehaviour
     {
         resources[resource] += amount;
         Debug.Log(resource + " : " + resources[resource]);
+    }
+
+    public string printResource(ResourceType resource)
+    {
+        string resString = "";
+        if (resource == ResourceType.Metal)
+        {
+            resString += "Metal: ";
+        }
+        else if (resource == ResourceType.Water)
+        {
+            resString += "Water: ";
+        }
+        else if (resource == ResourceType.Energy)
+        {
+            resString += "Energy: ";
+        }
+        
+        return resString + resources[resource];
     }
 }
